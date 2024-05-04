e8cdf939bdae9ed43da87265055eb5516bb734ba
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:45:31 -0300

    atividade completa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear(); 
+
+int primeiroNumero, segundoNumero;
+
+Console.WriteLine("--- Maior 2 Numeros ---");
+
+Console.Write("Digite o primeiro numero: ");
+
+primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero: ");
+
+segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+if(primeiroNumero > segundoNumero)
+{
+    Console.WriteLine($"o maior numero é {primeiroNumero}");
+}
+else 
+{
+    Console.WriteLine($"o maior numero é {segundoNumero}");
+}
